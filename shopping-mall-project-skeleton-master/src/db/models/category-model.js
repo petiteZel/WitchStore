9f9ff131c8ef6ed4c073aca3a12ebfcddd849c00
갑열 주 <cjswoduf@gmail.com>
2022-11-05 11:46:27 +0900

    Feat: category-model.js 파일 작성

---
+++ shopping-mall-project-skeleton-master/src/db/models/category-model.js
@@ -0,0 +1,47 @@
+import { model } from 'mongoose';
+import { CategorySchema } from '../schemas/category-schema';
+
+const Category = model('category', CategorySchema);
+
+export class CategoryModel {
+  // 카테고리 상세
+  async findByCategoryId(categoryId) {
+    const category = await Category.findOne({ _id: categoryId });
+    return category;
+  }
+  // 카테고리명으로 찾기
+  async findByCategoryName(categoryName) {
+    const category = await Category.findOne({ categoryName });
+    return category;
+  }
+  // 카테고리 추가
+  async create(categoryInfo) {
+    const createdNewCategory = await Category.create(categoryInfo);
+    return createdNewCategory;
+  }
+  // 전체 카테고리 조회
+  async findAll() {
+    const categories = await Category.find({});
+    return categories;
+  }
+  // 카테고리 수정
+  async update({ categoryId, update }) {
+    const filter = { _id: categoryId };
+    const option = { returnOriginal: false };
+    const updatedCategory = await Category.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return updatedCategory;
+  }
+  // 카테고리 삭제
+  async delete(categoryId) {
+    const category = await Category.findOneAndDelete({ _id: categoryId });
+    return category;
+  }
+}
+
+const categoryModel = new CategoryModel();
+
+export { categoryModel };
