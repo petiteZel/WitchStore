d4346b2bcae0a9d2befac4bfcb28f2f2770a4de7
갑열 주 <cjswoduf@gmail.com>
2022-11-05 22:37:09 +0900

    Feat: categoryModel 상품 추가 및 삭제시 카테고리 반영 기능

--- shopping-mall-project-skeleton-master/src/db/models/category-model.js
+++ shopping-mall-project-skeleton-master/src/db/models/category-model.js
@@ -9,39 +9,59 @@ export class CategoryModel {
     const newCategory = await Category.create(categoryInfo);
     return newCategory;
   }
-  // 2. 카테고리 검색 - 이름
+
+  // 2. 카테고리 삭제
+  async deleteCategory(categoryId){
+    const deleteCategory = await Category.deleteOne({_id: categoryId});
+    return deleteCategory;
+  }
+
+  // 3. 카테고리 id로 특정 카테고리 찾기
+  async findByCategoryId(categoryId) {
+    const category = await Category.findOne({ _id: categoryId });
+    return category;
+  }
+
+  // 4. 카테고리명으로 특정 카테고리 찾기
   async findByCategoryName(categoryName) {
     const category = await Category.findOne({ categoryName });
     return category;
   }
-  // 3. 카테고리 검색 - id
-  async findByCategoryId(categoryId) {
-    const category = await Category.findById(categoryId)
-    return category;
-  }
-  // 4. 전체 카테고리 조회
+
+  // 5. 전체 카테고리 조회
   async findAllCategories() {
     const categories = await Category.find({});
     return categories;
   }
-  // 5. 카테고리 수정
-  async updateCategory({ categoryId, update }) {
-    const filter = { _id: categoryId };
-    const option = { returnOriginal: false };
-    const updatedCategory = await Category.findOneAndUpdate(
-      filter,
-      update,
-      option
-    );
+
+  // 6. 카테고리 수정
+  async updateCategory({categoryId, name}){
+    const filter = {_id: categoryId};
+    const update = {name: name};
+    const option = { returnOriginal : false };
+    const updateCategory = await Category.updateOne(filter, update, option);
+    return updateCategory;
+  }
+
+  // 7. 상품 추가시 카테고리에 반영
+  async addProductToCategory(categoryName, productId){
+    const filter = { name: categoryName};
+    const update = { $push : { products : productId}};
+    const option = { returnOriginal : false };
+    const updatedCategory = await Category.updateOne(filter, update, option);
     return updatedCategory;
   }
-  // 6. 카테고리 삭제
-  async deleteCategory(categoryId) {
-    const category = await Category.findOneAndDelete({ _id: categoryId });
-    return category;
+
+  // 8. 상품 삭제 시 카테고리에 반영
+  async removeProductFromCategory(categoryName, productId){
+    const filter = { name: categoryName};
+    const update = { $pull : { products : productId}};
+    const option = { returnOriginal : false };
+    const updatedCategory = await Category.updateOne(filter, update, option);
+    return updatedCategory;
   }
 }
 
 const categoryModel = new CategoryModel();
 
-export { categoryModel };
+export { categoryModel }
