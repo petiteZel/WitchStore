b4e75b2befcccc121ad0424b797d234491dec004
갑열 주 <cjswoduf@gmail.com>
2022-11-06 06:00:14 +0000

    Fix: 주석 및 줄바꿈 일부 수정

--- shopping-mall-project-skeleton-master/src/services/order-service.js
+++ shopping-mall-project-skeleton-master/src/services/order-service.js
@@ -12,7 +12,7 @@ class OrderService {
       orderer: {
         userId: orderInfo.ordererUserId,
       },
-    // 1-2. 수령지
+    // 1-2. 수령자 정보
       recipient: {
         fullName: orderInfo.recipientFullName,
         phoneNumber: orderInfo.recipientPhoneNumber,
@@ -27,7 +27,6 @@ class OrderService {
     };
     // 1-4. db에 저장
     const createdNewOrder = await this.orderModel.create(newOrderInfo);
-
     return createdNewOrder;
   }
 
@@ -47,6 +46,7 @@ class OrderService {
 
     return orders;
   }
+
   // 4. 주문 번호로 주문 정보 조회
   async getOrdersByOrderId(orderId) {
     const orders = await this.orderModel.findByOrderId(orderId);
@@ -62,25 +62,22 @@ class OrderService {
   // 5. 주문 정보 수정
   async setOrderStatus(orderId, toUpdate) {
     let order = this.orderModel.findByOrderId(orderId);
-
-    // db에서 찾지 못한 경우, 에러 메시지 반환
+    // 5-1. db에서 찾지 못한 경우, 에러 메시지 반환
     if (!order) {
       throw new Error('주문 내역이 없습니다. 주문 번호를 확인해 주세요.');
     }
-
-    // 업데이트 진행
+    // 5-2. 업데이트 진행
     order = await this.orderModel.update({
       orderId,
       update: toUpdate,
     });
-
     return order;
   }
 
   // 6. 주문 정보 삭제 (주문 취소)
   async cancelOrder(orderId) {
     const order = await this.orderModel.findByOrderId(orderId);
-    // db에서 찾지 못한 경우, 에러 메시지 반환
+    // 6-1. db에서 찾지 못한 경우, 에러 메시지 반환
     if (!order) {
       throw new Error(
         '해당 주문 정보가 없습니다. 유효한 주문 정보가 아닙니다.'
@@ -93,5 +90,4 @@ class OrderService {
 }
 
 const orderService = new OrderService(orderModel);
-
 export { orderService };
