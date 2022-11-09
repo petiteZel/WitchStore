9d6f440dd7434dd0e600e9ae83b48b3f5db9d539
petiteZel <2882loverjqnrdl@gmail.com>
2022-11-09 20:11:01 +0900

    product-detail confirm 함수

--- shopping-mall-project-skeleton-master/src/views/product-detail/product-detail.js
+++ shopping-mall-project-skeleton-master/src/views/product-detail/product-detail.js
@@ -1,15 +1,28 @@
 import * as Api from "../api.js"
 import {products} from "../product-list/product-data.js"
 
+const user = require('../../db/models/user-model')
+console.log(user)
+
+function cartIn(key, value){
+    localStorage.setItem(key, value)
+    if(confirm("장바구니로 이동하시겠습니까?")){
+        location.href='/shopping-mall-project-skeleton-master/src/views/shopping-cart/shopping-cart.html'
+    }
+}
+
 async function productDetail(){
+
     const detailContainer = document.querySelector('.detail-item-container');
     const urlSearch = new URLSearchParams(location.search);
     const itemId = urlSearch.get("id")
     
+    // const products = await Api.get('url')
+    
     function findData(it,itemId){
         return it.id==itemId;
     }
-
+    
     const selectItem = products.find(e=>findData(e,itemId))
     const {
         id,
@@ -20,7 +33,7 @@ async function productDetail(){
         price,
         description,
         personType,
-      } = selectItem
+    } = selectItem
     
     detailContainer.innerHTML = `<div class="detail-item-img">
     <img src="${image}" />
@@ -29,46 +42,47 @@ async function productDetail(){
     <div class="store"><h3>${brand}</h3></div>
     
     <div class="item-info">
-      <div class="info-box" id="item-name">
-        <div class="info-title">상품명</div>
-        <div class="info-content">${productName}</div>
-      </div>
-      <div class="info-box" id="item-info-detail">
-        <div class="info-title">설명</div>
-        <div class="info-content">
-          ${description}
-        </div>
-      </div>
+    <div class="info-box" id="item-name">
+    <div class="info-title">상품명</div>
+    <div class="info-content">${productName}</div>
     </div>
-
+    <div class="info-box" id="item-info-detail">
+    <div class="info-title">설명</div>
+    <div class="info-content">
+    ${description}
+    </div>
+    </div>
+    </div>
+    
     <div class="item-info">
       <div class="info-box" id="item-price">
         <div class="info-title">가격</div>
         <div class="info-content">${price}</div>
         
         <div class="count">
-          <button class="plus-btn">
+        <button class="plus-btn">
             <i class="fa-solid fa-circle-plus"></i>
-          </button>
-          <div id="count-number">0</div>
-          <button class="minus-btn">
+            </button>
+            <div id="count-number">0</div>
+            <button class="minus-btn">
             <i class="fa-solid fa-circle-minus"></i>
-          </button>
+            </button>
         </div>
-      </div>
+        </div>
     </div>
     
     <div class="item-info">
-      <div class="info-box" id="total-price">
-        <div class="info-title">합계</div>
-        <div class="info-content">36,000</div>
-      </div>
+    <div class="info-box" id="total-price">
+    <div class="info-title">합계</div>
+    <div class="info-content">36,000</div>
+    </div>
     </div>
     
     <div class="cart-btn-box">
-      <button class="cart-btn">장바구니 담기</button>
+    <button class="cart-btn" onclick="${await cartIn(id,selectItem)}">장바구니 담기</button>
     </div>
-  </div>`
+    </div>`
+    
 }
 
 productDetail();
