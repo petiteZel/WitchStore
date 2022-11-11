4626e28a158beb2630d15c0f9dfb3aea4cfa3808
petiteZel <2882loverjqnrdl@gmail.com>
2022-11-11 18:13:32 +0900

    add-product 완전 끝

--- shopping-mall-project-skeleton-master/src/views/add-product/add-product.js
+++ shopping-mall-project-skeleton-master/src/views/add-product/add-product.js
@@ -1,6 +1,15 @@
 import { addImageToS3, getImageUrl } from "../awsS3.js"
 import * as Api from "../api.js"
 
+async function rending(){
+    const api = await Api.get('/api/category/categories')
+    const categoryBox = document.querySelector('#content-category')
+    api.forEach(e=>{
+        categoryBox.innerHTML += `<option value="${e.categoryName}">${e.categoryName}</option>`
+    })
+}
+rending()
+
 async function addCategory(){
     const submitBtn = document.querySelector('.submit-button');
     submitBtn.addEventListener('click',(e)=>submitInfo(e))
@@ -18,10 +27,8 @@ async function imageUpload() {
       fileName.innerHTML = file.name;
     }
   }
-changeImageName()
-
-async function submitInfo(e){
-    // e 추가
+  
+  async function submitInfo(e){
     await e.preventDefault();
     const productName = document.querySelector('#content-productName').value
     const category = document.querySelector('#content-category').value
@@ -32,7 +39,7 @@ async function submitInfo(e){
     const shortDescription = document.querySelector('#content-shortDescription').value
     const price = document.querySelector('#content-price').value
     const image = uploading.files[0]
-
+    
     try {
         const imageKey = await addImageToS3(uploading, category)
         const imageUrl = await getImageUrl(imageKey)
@@ -49,15 +56,16 @@ async function submitInfo(e){
         }
         await Api.post('/api/product/register',data)
         console.log(data)
-
+        
         alert(`정상적으로 ${productName}이 등록되었습니다.`)
     }catch (err) {
         console.log(err.stack);
-
+        
         alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`)
     }
 
 }
 
+changeImageName()
 changeImageName();
 addCategory()
