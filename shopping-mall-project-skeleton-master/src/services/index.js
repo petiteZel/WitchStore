a2fa50fdd7c2c09aa5a561c711fccaf837ffe455
kailey <kailey.lee224@gmail.com>
2022-11-12 16:04:50 +0900

    clean

--- shopping-mall-project-skeleton-master/src/services/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./user-service";
-export * from "./product-service";
-export * from "./category-service";
-export * from "./order-service";
-export * from "./order-item-service";
--- shopping-mall-project-skeleton-master/src/views/finish-shopping/finish-shopping.js
+++
@@ -1,5 +0,0 @@
-import * as Api from "../api.js";
-import { checkAdmin, createNavbar } from "../useful-functions.js";
-
-checkAdmin();
-createNavbar();
