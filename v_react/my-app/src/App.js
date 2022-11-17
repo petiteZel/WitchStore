6339a277f5c0cf963b6af251719f167b6466f307
petiteZel <2882loverjqnrdl@gmail.com>
2022-11-18 00:12:13 +0900

    react type, home 이동

--- v_react/my-app/src/App.js
+++ v_react/my-app/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import { BrowserRouter,Routes, Route, Link,Switch } from "react-router-dom";
 import Home from './component/home/home';
 import Type from './component/type-test/type-test';
+import Header from './component/header/header';
 
 function App() {
   return(
-  <div>
-    <Home></Home>
-    <Type></Type>
-    
-    
-    
-    
-  </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/type-test" element={<Type />} />
+      </Routes>
+      
+  </BrowserRouter>
   )
 }
 
