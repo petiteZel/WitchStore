bfa9d62727649169b05a2c4d8faaa36c9ca02a76
petiteZel <2882loverjqnrdl@gmail.com>
2022-11-24 01:08:10 +0900

    footer 삽입

--- v_react/my-app/src/component/footer/footer.js
+++ v_react/my-app/src/component/footer/footer.js
@@ -9,11 +9,11 @@ import style from "./footer.module.css"
 
 function Footer(){
     return(
-        <footer id="footer">
-        <div class="footer-container">
+        <footer id={style.footer}>
+        <div className={style.footerContainer}>
             
-            <div class="footer-box">
-                <div class="footer-copy-box">
+            <div className={style.footerBox}>
+                <div className={style.footerCopyBox}>
                     <p>
                         서울특별시 강남구 선릉로 433, 신관 6층<br />
                          team K1ng의 모든 컨텐츠는 저작권의 보호를 받고 있습니다.<br />
@@ -21,10 +21,10 @@ function Footer(){
                     </p>
                 </div>
     
-                <div class="footer-sns-box">
-                    <a href="#"><i class="fa-brands fa-square-youtube"></i></a>
-                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+                <div className={style.footerSnsBox}>
+                    <a href="#"><i className="fa-brands fa-square-youtube"></i></a>
+                    <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i className="fa-brands fa-square-facebook"></i></a>
                 </div>
             </div>
             
